77927ee37cc218be92be5171bd394ada8e2ce45c
oros@tk34.mskobr.ru <oros@tk34.mskobr.ru>
2022-04-25 12:02:47 +0300

    Добавьте файлы проекта.

---
+++ massiv/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace massiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] june = new int[1000];
+            int[] jule = new int[1000];
+            int sum = 0;
+            Random h = new Random();
+            for (int i = 0; i < june.Length; i++)
+            {
+                june[i] = h.Next();
+            }
+            for (int i = 0; i < jule.Length; i++)
+            {
+                jule[i] = h.Next();
+            }
+            for (int i = 0; i < jule.Length; i++)
+            {
+                sum = jule[i] + june[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
